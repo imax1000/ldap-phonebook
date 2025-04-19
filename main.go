e7c235c24fa6964d472c30030025e68c137e9f4e
Maxim Home(VM-RedOS7) <imax@local>
2025-04-19 20:52:07 +0700

    Version 0.0.1

--- main.go
+++ main.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"strconv"
 
-	"github.com/gotk3/gotk3/gtk"
+	"github.com/dawidd6/go-appindicator"
 	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
 	"gopkg.in/ldap.v2"
 )
 
-// App структура для хранения состояния приложения
+const (
+	appName    = "ldap-phonebook"
+	appID      = "com.example.ldap_phonebook"
+	appVersion = "1.0"
+	lockFile   = "/tmp/ldap-phonebook.lock"
+)
+
 type App struct {
 	window      *gtk.Window
-	statusIcon  *gtk.StatusIcon
-	trayMenu    *gtk.Menu
+	indicator   *appindicator.Indicator
 	isInTray    bool
 	ldapConn    *ldap.Conn
 	searchEntry *gtk.Entry
@@ -22,63 +30,95 @@ type App struct {
 }
 
 func main() {
-	// Инициализация GTK
+	// Проверка на уже запущенный экземпляр
+	if isAlreadyRunning() {
+		log.Println("Приложение уже запущено. Активируем существующее окно...")
+		//	activateExistingInstance()
+		//		os.Exit(0)
+	}
+
+	// Создаем lock-файл
+	if err := createLockFile(); err != nil {
+		log.Fatal("Не удалось создать lock-файл:", err)
+	}
+	defer os.Remove(lockFile)
+
 	gtk.Init(&os.Args)
 
-	// Создание экземпляра приложения
 	app := &App{}
-
-	// Создание главного окна
 	app.createMainWindow()
+	app.createAppIndicator()
 
-	// Создание иконки в трее
-	app.createStatusIcon()
-
-	// Запуск главного цикла GTK
 	gtk.Main()
+}
+
+func isAlreadyRunning() bool {
+	// Проверяем существование lock-файла
+	if _, err := os.Stat(lockFile); err == nil {
+		// Читаем PID из файла
+		data, err := os.ReadFile(lockFile)
+		if err != nil {
+			return false
+		}
+
+		// Проверяем существует ли процесс с этим PID
+		s := fmt.Sprintf("%s", data)
+		pid, err := strconv.Atoi(s)
+		if _, err := os.FindProcess(int(pid)); err == nil {
+			return true
+		}
+	}
+	return false
+}
+
+func createLockFile() error {
+	file, err := os.Create(lockFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("%d", os.Getpid()))
+	return err
+}
+
+func activateExistingInstance() {
+	cmd := exec.Command(os.Args[0], "--activate")
+	cmd.Start()
 }
 
 func (app *App) createMainWindow() {
 	var err error
 
-	// Создание главного окна
 	app.window, err = gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
 	if err != nil {
 		log.Fatal("Не удалось создать окно:", err)
 	}
 
-	app.window.SetTitle("LDAP Телефонный Справочник (Go)")
+	app.window.SetTitle(fmt.Sprintf("LDAP Телефонный Справочник v%s", appVersion))
 	app.window.SetDefaultSize(800, 600)
 	app.window.SetPosition(gtk.WIN_POS_CENTER)
+	app.window.SetIconName("system-users")
 
-	// Обработчик закрытия окна (будем сворачивать в трей)
+	// Обработчик закрытия окна (сворачиваем в трей)
 	app.window.Connect("delete-event", func() bool {
 		app.minimizeToTray()
-		return true // Предотвращаем закрытие окна
+		return true
 	})
 
-	// Основной интерфейс
 	app.createUI()
-
-	// Показываем все виджеты
 	app.window.ShowAll()
 }
 
 func (app *App) createUI() {
-	// Вертикальный контейнер
 	box, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 6)
 	if err != nil {
 		log.Fatal("Не удалось создать контейнер:", err)
 	}
 	app.window.Add(box)
 
-	// Панель подключения
 	app.createConnectionPanel(box)
-
-	// Панель поиска
 	app.createSearchPanel(box)
-
-	// Отображение результатов
 	app.createResultsView(box)
 }
 
@@ -101,21 +141,21 @@ func (app *App) createConnectionPanel(box *gtk.Box) {
 	grid.SetMarginEnd(6)
 	frame.Add(grid)
 
-	// Элементы для подключения
 	serverEntry, _ := gtk.EntryNew()
-	serverEntry.SetText("ldap://localhost")
+	serverEntry.SetText("localhost:389")
 
 	bindEntry, _ := gtk.EntryNew()
-	bindEntry.SetText("cn=admin,dc=example,dc=com")
+	bindEntry.SetText("cn=admin,dc=example,dc=org")
 
 	passwordEntry, _ := gtk.EntryNew()
+	passwordEntry.SetText("123456")
 	passwordEntry.SetVisibility(false)
 
 	connectBtn, _ := gtk.ButtonNewWithLabel("Подключиться")
 	connectBtn.Connect("clicked", func() {
-		server := serverEntry.GetText()
-		bindDN := bindEntry.GetText()
-		password := passwordEntry.GetText()
+		server, _ := serverEntry.GetText()
+		bindDN, _ := bindEntry.GetText()
+		password, _ := passwordEntry.GetText()
 
 		conn, err := ldap.Dial("tcp", server)
 		if err != nil {
@@ -133,7 +173,6 @@ func (app *App) createConnectionPanel(box *gtk.Box) {
 		app.showMessage("Успешно подключено к LDAP серверу")
 	})
 
-	// Размещение элементов
 	grid.Attach(createLabel("Сервер:"), 0, 0, 1, 1)
 	grid.Attach(serverEntry, 1, 0, 1, 1)
 	grid.Attach(createLabel("Учетная запись:"), 0, 1, 1, 1)
@@ -168,20 +207,17 @@ func (app *App) createResultsView(box *gtk.Box) {
 	scrolled, _ := gtk.ScrolledWindowNew(nil, nil)
 	box.PackStart(scrolled, true, true, 0)
 
-	// Создаем модель данных
 	app.listStore, _ = gtk.ListStoreNew(
-		glib.TYPE_STRING, // ФИО
-		glib.TYPE_STRING, // Должность
-		glib.TYPE_STRING, // Отдел
-		glib.TYPE_STRING, // Телефон
+		glib.TYPE_STRING,
+		glib.TYPE_STRING,
+		glib.TYPE_STRING,
+		glib.TYPE_STRING,
 	)
 
-	// Создаем TreeView
 	treeView, _ := gtk.TreeViewNewWithModel(app.listStore)
 	treeView.SetHeadersVisible(true)
 	scrolled.Add(treeView)
 
-	// Добавляем колонки
 	addColumn(treeView, "ФИО", 0)
 	addColumn(treeView, "Должность", 1)
 	addColumn(treeView, "Отдел", 2)
@@ -194,35 +230,42 @@ func (app *App) onSearchClicked() {
 		return
 	}
 
-	searchTerm := app.searchEntry.GetText()
-	if searchTerm == "" {
-		app.showError("Ошибка", "Введите поисковый запрос")
-		return
-	}
+	searchTerm, _ := app.searchEntry.GetText()
+	//	if searchTerm == "" {
+	//		app.showError("Ошибка", "Введите поисковый запрос")
+	//		return
+	//	}
 
-	// Очищаем предыдущие результаты
 	app.listStore.Clear()
 
-	// Выполняем поиск
+	str := fmt.Sprintf("(&(objectClass=person)(|(cn=*%s*)(sn=*%s*)))", searchTerm, searchTerm)
+	//	str = "(objectClass=person)"
 	searchRequest := ldap.NewSearchRequest(
-		"dc=example,dc=com",
+		"dc=example,dc=org",
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		fmt.Sprintf("(|(cn=*%s*)(telephoneNumber=*%s*))", searchTerm, searchTerm),
-		[]string{"cn", "title", "department", "telephoneNumber"},
+		str,
+		[]string{"cn", "title", "ou", "telephoneNumber"},
 		nil,
 	)
+	//	searchRequest := ldap.NewSearchRequest(
+	//		"dc=example,dc=com",
+	//		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+	//		fmt.Sprintf("(&(objectClass=user)(|(cn=*%s*)(sn=*%s*)))", searchTerm, searchTerm),
+	//		[]string{"cn", "title", "department", "telephoneNumber"},
+	//		nil,
+	//	)
 
 	result, err := app.ldapConn.Search(searchRequest)
 	if err != nil {
-		app.showError("Ошибка поиска", err.Error())
+		str := err.Error()
+		app.showError("Ошибка поиска", str)
 		return
 	}
 
-	// Заполняем результаты
 	for _, entry := range result.Entries {
 		name := entry.GetAttributeValue("cn")
 		title := entry.GetAttributeValue("title")
-		dept := entry.GetAttributeValue("department")
+		dept := entry.GetAttributeValue("ou")
 		phone := entry.GetAttributeValue("telephoneNumber")
 
 		iter := app.listStore.Append()
@@ -235,74 +278,51 @@ func (app *App) onSearchClicked() {
 	app.showMessage(fmt.Sprintf("Найдено %d записей", len(result.Entries)))
 }
 
-// Создание иконки в трее
-func (app *App) createStatusIcon() {
-	statusIcon, err := gtk.StatusIconNew()
-	if err != nil {
-		log.Fatal("Не удалось создать иконку в трее:", err)
-	}
-	app.statusIcon = statusIcon
+func (app *App) createAppIndicator() {
+	app.indicator = appindicator.New(appName, "system-users", appindicator.CategoryApplicationStatus)
+	app.indicator.SetStatus(appindicator.StatusActive)
 
-	// Устанавливаем иконку (должна быть в путях или указать полный путь)
-	app.statusIcon.SetFromIconName("system-users")
-	app.statusIcon.SetTooltipText("LDAP Телефонный Справочник")
+	menu, _ := gtk.MenuNew()
 
-	// Обработчик клика по иконке
-	app.statusIcon.Connect("activate", func() {
-		if app.isInTray {
-			app.restoreFromTray()
-		} else {
-			app.minimizeToTray()
-		}
-	})
-
-	// Создаем контекстное меню для иконки в трее
-	app.createTrayMenu()
-}
-
-// Создание меню для иконки в трее
-func (app *App) createTrayMenu() {
-	menu, err := gtk.MenuNew()
-	if err != nil {
-		log.Fatal("Не удалось создать меню:", err)
-	}
-	app.trayMenu = menu
-
-	// Пункт "Показать"
 	showItem, _ := gtk.MenuItemNewWithLabel("Показать")
 	showItem.Connect("activate", app.restoreFromTray)
 	menu.Append(showItem)
 
-	// Разделитель
 	separator, _ := gtk.SeparatorMenuItemNew()
 	menu.Append(separator)
 
-	// Пункт "Выход"
 	exitItem, _ := gtk.MenuItemNewWithLabel("Выход")
 	exitItem.Connect("activate", func() {
+		os.Remove(lockFile)
 		gtk.MainQuit()
 	})
 	menu.Append(exitItem)
 
 	menu.ShowAll()
+	app.indicator.SetMenu(menu)
 }
 
-// Сворачивание в трей
 func (app *App) minimizeToTray() {
 	app.window.Hide()
 	app.isInTray = true
-	app.statusIcon.SetVisible(true)
 }
 
-// Восстановление из трея
 func (app *App) restoreFromTray() {
-	app.window.Present()
-	app.window.Show()
-	app.isInTray = false
-	app.statusIcon.SetVisible(false)
+	if app.window != nil {
+		app.window.Present()
+		app.window.Show()
+		app.window.Deiconify()
+		app.window.SetKeepAbove(true)
+
+		glib.TimeoutAdd(100, func() bool {
+			app.window.SetKeepAbove(false)
+			return false
+		})
+
+		app.isInTray = false
+	}
 }
 
-// Вспомогательные функции
 func createLabel(text string) *gtk.Label {
 	label, err := gtk.LabelNew(text)
 	if err != nil {
@@ -316,7 +336,7 @@ func addColumn(treeView *gtk.TreeView, title string, id int) {
 	renderer, _ := gtk.CellRendererTextNew()
 	column, _ := gtk.TreeViewColumnNewWithAttribute(title, renderer, "text", id)
 	column.SetResizable(true)
-	column.SetSortColumnId(id)
+	column.SetSortColumnID(id)
 	treeView.AppendColumn(column)
 }
 
@@ -334,6 +354,14 @@ func (app *App) showError(title, message string) {
 }
 
 func (app *App) showMessage(message string) {
-	// Можно реализовать через статусбар или уведомление
-	log.Println(message)
+	dialog := gtk.MessageDialogNew(
+		app.window,
+		gtk.DIALOG_MODAL,
+		gtk.MESSAGE_INFO,
+		gtk.BUTTONS_OK,
+		message,
+	)
+	dialog.SetTitle("Сообщение")
+	dialog.Run()
+	dialog.Destroy()
 }
